d5f752a1c11d6a92c74d2e7daf09198bc762f291
Max Liu <mjliu@mit.edu>
2020-03-04 15:22:34 -0500

    Additional unit tests for Pricer

--- makeit/utilities/buyable/pricer_test.py
+++ makeit/utilities/buyable/pricer_test.py
@@ -1,20 +1,49 @@
+import gzip
+import os
 import unittest
-import makeit.utilities.buyable.pricer as p
+
+from makeit.utilities.buyable.pricer import Pricer
+
 
 class TestPricer(unittest.TestCase):
-    def setUp(self):
-        self.pricer = p.Pricer()
-        self.pricer.load()
 
-    def test_01_lookup_smiles(self):
+    @classmethod
+    def setUpClass(cls):
+        """This method is run once before every test in this module."""
+        cls.pricer = Pricer()
+        # Set some arbitrary prices
+        cls.pricer.prices['CCCCCO'] = 1.0
+        cls.pricer.prices['c1ccccc1'] = 2.0
+
+    def test_lookup_existing_smiles(self):
+        """Test that we can lookup a SMILES that exists."""
         result = self.pricer.lookup_smiles('CCCCCO')
         expected = 1.0
         self.assertAlmostEqual(expected, result)
 
-    def test_02_lookup_smiles(self):
+    def test_lookup_nonexisting_smiles(self):
+        """Test that we can lookup a SMILES that does not exist."""
         result = self.pricer.lookup_smiles('CCCCXCCO')
         expected = 0.0
         self.assertAlmostEqual(expected, result)
+
+    def test_dump_and_load(self):
+        """Test that we can dump the prices to a file and load again."""
+        filename = 'temp.json.gz'
+        self.pricer.dump_to_file(filename)
+        self.assertTrue(os.path.isfile(filename))
+
+        with gzip.open(filename, 'rt', encoding='utf-8') as f:
+            # Just check the length of the string, since dict order is non-deterministic
+            self.assertEqual(len(f.read()), 70)
+
+        new_pricer = Pricer()
+        new_pricer.load_from_file(filename)
+
+        self.assertEqual(self.pricer.prices, new_pricer.prices)
+
+        os.remove(filename)
+
 
 if __name__ == '__main__':
     res = unittest.main(verbosity=3, exit=False)

8ef3b0c8685b884d2abf837f0fdb9b454c797220
Max Liu <mjliu@mit.edu>
2020-03-04 14:36:02 -0500

    Minor code style changes to pricer module

--- makeit/utilities/buyable/pricer.py
+++ makeit/utilities/buyable/pricer.py
@@ -1,22 +1,22 @@
 import gzip
 import json
-import makeit.global_config as gc
-import rdkit.Chem as Chem
-from collections import defaultdict
-from tqdm import tqdm
-from makeit.utilities.io.logger import MyLogger
-import makeit.utilities.io.pickle as pickle
-from pymongo import MongoClient, errors
-from multiprocessing import Manager
 import os
+from collections import defaultdict
+
+import rdkit.Chem as Chem
+from pymongo import MongoClient, errors
+
+import makeit.global_config as gc
+from makeit.utilities.io.logger import MyLogger
+
 pricer_loc = 'pricer'
 
 
 class Pricer:
-    '''
+    """
     The Pricer class is used to look up the ppg of chemicals if they
     are buyable.
-    '''
+    """
 
     def __init__(self, use_db=False, BUYABLES_DB=None):
 
@@ -25,10 +25,10 @@ class Pricer:
         self.prices = defaultdict(float)  # default 0 ppg means not buyable
 
     def load(self, file_name=gc.BUYABLES['file_name']):
-        '''
+        """
         Load pricer information. Either create connection to MongoDB or load from local file.
         If connection to MongoDB cannot be made, fallback and try to load from local file.
-        '''
+        """
         if self.use_db:
             self.load_databases()
             return
@@ -40,13 +40,13 @@ class Pricer:
         self.load_from_file(file_name)
 
     def load_databases(self):
-        '''
+        """
         Load the pricing data from the online database
-        '''
+        """
         db_client = MongoClient(
-            gc.MONGO['path'], 
+            gc.MONGO['path'],
             gc.MONGO['id'],
-            connect=gc.MONGO['connect'], 
+            connect=gc.MONGO['connect'],
             serverSelectionTimeoutMS=1000
         )
 
@@ -57,14 +57,14 @@ class Pricer:
             self.use_db = False
             self.load()
             return
-        
+
         db = db_client[gc.BUYABLES['database']]
         self.BUYABLES_DB = db[gc.BUYABLES['collection']]
 
     def dump_to_file(self, file_path):
-        '''
+        """
         Write prices to a local file
-        '''
+        """
         prices = []
         for k, v in self.prices.items():
             tmp = v.copy()
@@ -75,9 +75,9 @@ class Pricer:
             json.dump(prices, f)
 
     def load_from_file(self, file_name):
-        '''
+        """
         Load buyables information from local file
-        '''
+        """
         with gzip.open(file_name, 'rb') as f:
             prices = json.loads(f.read().decode('utf-8'))
 
@@ -88,13 +88,13 @@ class Pricer:
         MyLogger.print_and_log('Loaded prices from flat file', pricer_loc)
 
     def lookup_smiles(self, smiles, alreadyCanonical=False, isomericSmiles=True):
-        '''
+        """
         Looks up a price by SMILES. Canonicalize smiles string unless 
         the user specifies that the smiles string is definitely already 
         canonical. If the DB connection does not exist, look up from 
         prices dictionary attribute, otherwise lookup from DB.
         If multiple entries exist in the DB, return the lowest price.
-        '''
+        """
         if not alreadyCanonical:
             mol = Chem.MolFromSmiles(smiles)
             if not mol:

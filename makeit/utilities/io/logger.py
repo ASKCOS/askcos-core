77cd61b208acce4ea7b095e79a6f21518cd8f082
Max Liu <mjliu@mit.edu>
2020-04-01 15:28:48 -0400

    Clean up and minor improvements to logger.py
    Setting time_zero in global configuration is unnecessary

--- makeit/utilities/io/logger.py
+++ makeit/utilities/io/logger.py
@@ -1,6 +1,5 @@
 import os
 import time
-import makeit.global_config as gc
 
 
 def select_log_path(root='', name=''):
@@ -18,36 +17,41 @@ def select_log_path(root='', name=''):
 
 
 class MyLogger:
-    '''
+    """
     Create logger. Four different levels of information output. A level 3 ("FATAL")
     log will exit the program.
-    '''
-    logFile = select_log_path()
+    """
+    log_file = select_log_path()
     levels = {
         0: 'INFO',
         1: 'WARN',
         2: 'ERROR',
         3: 'FATAL'
     }
+    time_zero = 0
 
     @staticmethod
     def initialize_logFile(root='', name=''):
+        """Clear previous log file and set initialization time."""
         if name:
-            MyLogger.logFile = select_log_path(root, name)
-        if os.path.isfile(MyLogger.logFile):
-            os.remove(MyLogger.logFile)
-        gc.time_zero = time.time()
+            MyLogger.log_file = select_log_path(root, name)
+        if os.path.isfile(MyLogger.log_file):
+            os.remove(MyLogger.log_file)
+        MyLogger.time_zero = time.time()
 
     @staticmethod
     def print_and_log(text, location, level=0):
-        file = open(MyLogger.logFile, 'a')
-        time_elapsed = time.time() - gc.time_zero
+        """Print message to stdout and write to log file."""
+        time_elapsed = time.time() - MyLogger.time_zero
 
-        outstr = '{:25s}: [{:04.3f}s]\t{}'.format('{}@{}'.format(MyLogger.levels[level], location)[:25],
-            time_elapsed, text)
+        tag = '{}@{}'.format(MyLogger.levels[level], location)[:25]
+        outstr = '{:25s}: [{:04.3f}s]\t{}'.format(tag, time_elapsed, text)
+
         print(outstr)
-        file.write(outstr)
-        file.write('\n')
-        file.close()
+
+        with open(MyLogger.log_file, 'a') as f:
+            f.write(outstr)
+            f.write('\n')
+
         if level == 3:
             quit()
